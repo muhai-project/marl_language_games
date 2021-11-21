d7fef7d91381ead6611f4a5fbaf3bebd7dcffe1f
jerome <jerome@ai.vub.ac.be>
2021-11-22 00:46:20 +0100

    add: setup.py to install emrl package

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="emrl",
+    version="0.1.0",
+    url="https://gitlab.ai.vub.ac.be/ehai/emergent-rl",
+    author="Jérôme Botoko",
+    author_email="jerome@ai.vub.ac.be",
+    description="Emergent communication through LG and RL",
+    packages=find_packages(),
+)
