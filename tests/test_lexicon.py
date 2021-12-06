6ca2ba32104ca3635edad5a53ae3819bd8e01f82
jerome <jerome@ai.vub.ac.be>
2021-12-06 10:55:59 +0100

    refactor: move main script to dedicated scripts folder

--- tests/test_lexicon.py
+++ tests/test_lexicon.py
@@ -1,5 +1,5 @@
 from emrl.environment.lexicon import Lexicon
-from emrl.utils.utils import cfg_from_file
+from emrl.utils.cfg import cfg_from_file
 
 cfg = cfg_from_file("cfg/bng.yml")  # TODO only tests for bng cfg?!
 # http://qualityisspeed.blogspot.com/2015/02/the-dependency-elimination-principle-a-canonical-example.html
